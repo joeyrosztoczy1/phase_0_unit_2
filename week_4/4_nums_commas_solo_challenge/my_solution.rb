811293a1e9cbe63cbbca40de43c77b27d632a21b
Joey Rosztoczy <joeyr@stotzeq.com>
2014-04-27 12:56:21 -0700

    Reflected on solo challenge

--- week_4/4_nums_commas_solo_challenge/my_solution.rb
+++ week_4/4_nums_commas_solo_challenge/my_solution.rb
@@ -18,10 +18,9 @@
 # 2. Initial Solution
 
 def separate_comma(int)
-	return int.to_s.split("").reverse.each_slice(3).map { |e| e.join("").reverse  }.reverse.join(",")
+	return int.to_s.split("").reverse.each_slice(3).map { |i| i.join("").reverse  }.reverse.join(",")
 end
 
-separate_comma(100000)
 
 
 
@@ -31,3 +30,11 @@ separate_comma(100000)
 
 
 # 4. Reflection 
+#
+# I hugely enjoyed the logic challenge in this exercise. It was equally awesome because it was hard to find any documentation etc on the outcome I wanted via
+# google because everyone uses regex's when they want to achieve a similar outcome. I also enjoyed it because it was not one of the challenges that I
+# struggled for hours on, it came through relatively quick. I knew I wanted to use the each_slice, because I had used that method earlier in Phase 0.
+# The next step for me was to figure out how to count to the right indexes, and I quickly realized the only way to do it was from spinning around 
+# the contents, because than I could always be sure I was landing at a multiple of 3 with each slice. I also figured out one of my biggest general 
+# challenges in coding so far: defining local variables and trying to access them out of scope. My original try had me setting a bunch of variables, but
+# simply returning the answer has been much cleaner and less frustrating!
