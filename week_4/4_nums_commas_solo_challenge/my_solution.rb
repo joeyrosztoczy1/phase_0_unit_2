b33b52e159ca3cdec56363cee142b46ed5f29903
Joey Rosztoczy <joeyr@stotzeq.com>
2014-04-26 19:15:42 -0700

    solo_challenge BLASTED

--- week_4/4_nums_commas_solo_challenge/my_solution.rb
+++ week_4/4_nums_commas_solo_challenge/my_solution.rb
@@ -7,12 +7,21 @@
 
 # 1. Pseudocode
 
-# What is the input?
-# What is the output? (i.e. What should the code return?)
-# What are the steps needed to solve the problem?
+# What is the input? The input is an integer (that is all LOL)
+# What is the output? The output is the integer formatted with proper commas
+# What are the steps needed to solve the problem? 
+# (1) define a method that accepts an integer
+# (2) insert commas every three characters
+# (3) output the result
 
 
 # 2. Initial Solution
+
+def separate_comma(int)
+	return int.to_s.split("").reverse.each_slice(3).map { |e| e.join("").reverse  }.reverse.join(",")
+end
+
+separate_comma(100000)
 
 
 
